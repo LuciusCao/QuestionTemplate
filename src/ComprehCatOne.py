c0cc0f67a456b75edc98968f8a3d84230cfa8793
LuciusCao <lucius.cao@gmail.com>
2017-11-08 00:50:38 +0800

    first scratch of this template implementation in Jupyiter Notebook

--- src/ComprehCatOne.py
+++ src/ComprehCatOne.py
@@ -2,10 +2,16 @@ from QuestionTemplate import QuestionTemplate
 
 
 class ComprehCatOne(QuestionTemplate):
-    def __init__(self, name):
-        QuestionTemplate.__init__(self, name)
+    pass
 
 
 if __name__ == "__main__":
-    example = ComprehCatOne("this is name")
+    QUESTION_PARAMS = {
+        'name': 'Olympic Math Comprehensive Category One',
+        'question_type': 'Filling the blank',
+        'question_body': 'body',
+        'correct_answer': 2,
+        'answer_explanation': 'exp'
+    }
+    example = ComprehCatOne(QUESTION_PARAMS)
     example.generate()
--- src/QuestionTemplate.py
+++ src/QuestionTemplate.py
@@ -1,7 +1,35 @@
 class QuestionTemplate:
-    def __init__(self, name):
-        self.name = name
+    '''A question template class that helps generate questions'''
+    import random
+
+    def __init__(self, question_params, random_state=None):
+        self.name = question_params['name']
+        self.question_type = question_params['question_type']
+        self.question_body = question_params['question_body']
+        self.correct_answer = question_params['correct_answer']
+        self.answer_explanation = question_params['answer_explanation']
+
+        if random_state is not None:
+            self.random.seed(random_state)
+
+        if self.question_type == 'Multiple Choices':
+            self.choices = question_params['choices']
+        else:
+            self.choices = None
+
+    def show_meta(self):
+        '''show meta information about the question'''
+        print('name: {}'.format(self.name))
+        print('question type: {}'.format(self.question_type))
 
     def generate(self):
-        print("success {}".format(self.name))
+        '''generate a valid question via this template'''
+        print('Question Generated')
+        print('name: {}'.format(self.name))
+        print('question type: {}'.format(self.question_type))
+        print('question body: {}'.format(self.question_body))
+        print('correct_answer: {}'.format(self.correct_answer))
+        print('answer_explanation {}'.format(self.answer_explanation))
+        if self.question_type == 'Multiple Choices':
+            print('choices: {}'.format(self.choices))
 
